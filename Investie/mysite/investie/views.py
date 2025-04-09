99dfa5b285d9570656067d7a410bf225d261573d
MayaHA2512 <maya.hetti-arachchig@ada.ac.uk>
2025-04-09 20:50:53 +0100

    Updating the graph when ticker changes

--- Investie/mysite/investie/views.py
+++ Investie/mysite/investie/views.py
@@ -8,8 +8,8 @@ from django.http import JsonResponse
 from alpha_vantage.timeseries import TimeSeries
 
 def index(request):
-    stock_data = yf.Ticker("AAPL").history(period="6mo", auto_adjust=True)
-
+    ticker = request.GET.get('ticker', 'AAPL')
+    stock_data = yf.Ticker(ticker).history(period="6mo", auto_adjust=True)
     labels = stock_data.index.strftime('%Y-%m-%d').tolist()
     data = stock_data['Close'].tolist()
     chart_data = {
@@ -33,11 +33,21 @@ def entry_view(request):
 
 
 def get_live_price(request):
-    print(request)
     ticker = request.GET.get('ticker', 'AAPL')
     print(f'getting ticker for {ticker}')
+
     stock = yf.Ticker(ticker)
-    current_price = stock.history(period="1d")['Close'].iloc[0]
-    if current_price:
-        current_price = round(current_price, 2)
-    return JsonResponse({'current_price': current_price, 'last_updated': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+
+    today_data = stock.history(period="1d")
+    current_price = today_data['Close'].iloc[-1] if not today_data.empty else None
+
+    history_data = stock.history(period="1mo")
+    labels = history_data.index.strftime('%Y-%m-%d').tolist()
+    chart_data = history_data['Close'].tolist()
+
+    return JsonResponse({
+        'current_price': round(current_price) if current_price else "N/A",
+        'last_updated': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'chart_labels': labels,
+        'chart_data': chart_data
+    })
