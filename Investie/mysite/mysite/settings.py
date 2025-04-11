32b225f5ff936e7ecc76ec02a5c6b8f22954ef61
MayaHA2512 <maya.hetti-arachchig@ada.ac.uk>
2025-04-11 17:47:07 +0100

    new unit tests

--- Investie/mysite/mysite/settings.py
+++ Investie/mysite/mysite/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'investie.middleware.LogRequestMiddleware',
+    # 'investie.middleware.LogRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
